ab187506a299e4926a94cfbea1d7b126a3b6670b
Rich Lander <lander2k2@protonmail.com>
2023-04-30 15:48:48 -0400

    dev: implement code generation for controller reconcilers
    Signed-off-by: Rich Lander <lander2k2@protonmail.com>

--- cmd/workload-controller/main_gen.go
+++ cmd/workload-controller/main_gen.go
@@ -6,11 +6,6 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"net/http"
-	"os"
-	"sync"
-	"time"
-
 	logr "github.com/go-logr/logr"
 	zapr "github.com/go-logr/zapr"
 	uuid "github.com/google/uuid"
@@ -19,8 +14,13 @@ import (
 	version "github.com/threeport/threeport/internal/version"
 	workload "github.com/threeport/threeport/internal/workload"
 	v0 "github.com/threeport/threeport/pkg/api/v0"
+	config "github.com/threeport/threeport/pkg/config/v0"
 	controller "github.com/threeport/threeport/pkg/controller"
 	zap "go.uber.org/zap"
+	"net/http"
+	"os"
+	"sync"
+	"time"
 )
 
 func main() {
@@ -121,7 +121,7 @@ func main() {
 	var shutdownWait sync.WaitGroup
 
 	// configure http client for calls to threeport API
-	apiClient, err := clientInternal.GetHTTPClient(*authEnabled)
+	apiClient, err := config.GetHTTPClient(*authEnabled)
 	if err != nil {
 		log.Error(err, "failed to create http client")
 		os.Exit(1)
