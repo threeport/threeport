8b03c31122637cdea206d4e62c7b0cc60928dc58
Rich Lander <lander2k2@protonmail.com>
2023-05-16 19:39:12 -0400

    feat: add get command to tptctl
    Signed-off-by: Rich Lander <lander2k2@protonmail.com>

---
+++ cmd/tptctl/cmd/get_workload_definitions.go
@@ -0,0 +1,75 @@
+/*
+Copyright © 2023 Threeport admin@threeport.io
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"text/tabwriter"
+
+	"github.com/spf13/cobra"
+
+	"github.com/threeport/threeport/internal/cli"
+	"github.com/threeport/threeport/internal/util"
+	client "github.com/threeport/threeport/pkg/client/v0"
+	config "github.com/threeport/threeport/pkg/config/v0"
+)
+
+// GetWorkloadDefinitionsCmd represents the workload-definition command
+var GetWorkloadDefinitionsCmd = &cobra.Command{
+	Use:          "workload-definitions",
+	Example:      "tptctl get workload-definitions",
+	Short:        "Get workload definitions from the system",
+	Long:         `Get workload definitions from the system.`,
+	SilenceUsage: true,
+	Run: func(cmd *cobra.Command, args []string) {
+		// get threeport config and extract threeport API endpoint
+		threeportConfig, err := config.GetThreeportConfig()
+		if err != nil {
+			cli.Error("failed to get threeport config", err)
+			os.Exit(1)
+		}
+		apiEndpoint, err := threeportConfig.GetThreeportAPIEndpoint()
+		if err != nil {
+			cli.Error("failed to get threeport API endpoint from config", err)
+			os.Exit(1)
+		}
+
+		// get threeport API client
+		authEnabled, err = threeportConfig.GetThreeportAuthEnabled()
+		if err != nil {
+			cli.Error("failed to determine if auth is enabled on threeport API", err)
+			os.Exit(1)
+		}
+		ca, clientCertificate, clientPrivateKey, err := threeportConfig.GetThreeportCertificates()
+		if err != nil {
+			cli.Error("failed to get threeport certificates from config", err)
+			os.Exit(1)
+		}
+		apiClient, err := client.GetHTTPClient(authEnabled, ca, clientCertificate, clientPrivateKey)
+		if err != nil {
+			cli.Error("failed to create threeport API client", err)
+			os.Exit(1)
+		}
+
+		// get workload definitions
+		workloadDefinitions, err := client.GetWorkloadDefinitions(apiClient, apiEndpoint)
+		if err != nil {
+			cli.Error("failed to retrieve workload definitions", err)
+			os.Exit(1)
+		}
+
+		// write the output
+		writer := tabwriter.NewWriter(os.Stdout, 4, 4, 4, ' ', 0)
+		fmt.Fprintln(writer, "NAME\t AGE")
+		for _, wd := range *workloadDefinitions {
+			fmt.Fprintln(writer, *wd.Name, "\t", util.GetAge(wd.CreatedAt))
+		}
+		writer.Flush()
+	},
+}
+
+func init() {
+	getCmd.AddCommand(GetWorkloadDefinitionsCmd)
+}
