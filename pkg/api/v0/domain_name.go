a1b2501733ad08b298fe8cf6912b5e844ade1333
Randy Johnson <randy@qleet.io>
2023-07-26 18:08:07 -0400

    fix: cleanup prior to merge

--- pkg/api/v0/domain_name.go
+++ pkg/api/v0/domain_name.go
@@ -13,6 +13,16 @@ type DomainNameDefinition struct {
 
 	// The name of the zone in which the domain is managed.
 	Zone *string `json:"Zone,omitempty" query:"zone" gorm:"not null" validate:"required"`
+
+	// Whether or not the domain name is a top level domain.
+	// TopLevelDomain *bool `json:"TopLevelDomain,omitempty" query:"toplevel" gorm:"default:false" validate:"optional"`
+
+	// TTL configuration for this record.
+	// TTL *uint `json:"TTL,omitempty" query:"ttl" gorm:"default:300" validate:"optional"`
+
+	// The type of DNS record to create.
+	// Type *string `json:"Type,omitempty" query:"type" gorm:"default:'A'" validate:"optional"`
+
 }
 
 // DomainNameInstance is an instance of domain name management for a workload.
@@ -25,14 +35,5 @@ type DomainNameInstance struct {
 
 	// The cluster where the workload that is using the domain name is running.
 	ClusterInstanceID *uint `json:"ClusterInstanceID,omitempty" query:"clusterinstanceid" gorm:"not null" validate:"required"`
-
-	// Whether or not the domain name is a top level domain.
-	// TopLevelDomain *bool `json:"TopLevelDomain,omitempty" query:"toplevel" gorm:"default:false" validate:"optional"`
-
-	// TTL configuration for this record.
-	// TTL *uint `json:"TTL,omitempty" query:"ttl" gorm:"default:300" validate:"optional"`
-
-	// The type of DNS record to create.
-	// Type *string `json:"Type,omitempty" query:"type" gorm:"default:'A'" validate:"optional"`
 
 }
