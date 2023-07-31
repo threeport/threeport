15950f659ad53bdf2e6331ed807687080fd251ba
Randy Johnson <randy@qleet.io>
2023-07-31 15:48:12 -0400

    feat: implement domain name definition reconciler

--- internal/gateway/domain_name_definition.go
+++ internal/gateway/domain_name_definition.go
@@ -1,8 +1,11 @@
 package gateway
 
 import (
+	"fmt"
+
 	"github.com/go-logr/logr"
 	v0 "github.com/threeport/threeport/pkg/api/v0"
+	client "github.com/threeport/threeport/pkg/client/v0"
 	controller "github.com/threeport/threeport/pkg/controller/v0"
 )
 
@@ -13,6 +16,44 @@ func domainNameDefinitionCreated(
 	domainNameDefinition *v0.DomainNameDefinition,
 	log *logr.Logger,
 ) error {
+
+	// create domain name definition
+	dnsEndpoint, err := createDnsEndpoint()
+	if err != nil {
+		return fmt.Errorf("failed to create domain name: %w", err)
+	}
+
+	// construct workload definition object
+	workloadDefinition := v0.WorkloadDefinition{
+		Definition: v0.Definition{
+			Name: domainNameDefinition.Name,
+		},
+		YAMLDocument: &dnsEndpoint,
+	}
+
+	// create workload definition
+	createdWorkloadDefinition, err := client.CreateWorkloadDefinition(r.APIClient, r.APIServer, &workloadDefinition)
+	if err != nil {
+		return fmt.Errorf("failed to create workload definition in threeport API: %w", err)
+	}
+
+	// update gateway definition
+	domainNameDefinitionReconciled := true
+	domainNameDefinition.Reconciled = &domainNameDefinitionReconciled
+	domainNameDefinition.WorkloadDefinitionID = createdWorkloadDefinition.ID
+	_, err = client.UpdateDomainNameDefinition(
+		r.APIClient,
+		r.APIServer,
+		domainNameDefinition,
+	)
+	if err != nil {
+		return fmt.Errorf("failed to update gateway definition in threeport API: %w", err)
+	}
+
+	log.V(1).Info(
+		"domain name definition created",
+		"domainNameDefinitionID", domainNameDefinition.ID,
+	)
 
 	return nil
 }
@@ -25,6 +66,50 @@ func domainNameDefinitionUpdated(
 	log *logr.Logger,
 ) error {
 
+	// create domain name definition
+	dnsEndpoint, err := createDnsEndpoint()
+	if err != nil {
+		return fmt.Errorf("failed to create domain name: %w", err)
+	}
+
+	// get workload definition
+	if domainNameDefinition.WorkloadDefinitionID == nil {
+		return fmt.Errorf("failed to update workload definition, workload definition ID is nil")
+	}
+	workloadDefinition, err := client.GetWorkloadDefinitionByID(
+		r.APIClient,
+		r.APIServer,
+		*domainNameDefinition.WorkloadDefinitionID,
+	)
+	if err != nil {
+		return fmt.Errorf("failed to get workload definition by workload definition ID: %w", err)
+	}
+
+	// update workload definition
+	workloadDefinition.YAMLDocument = &dnsEndpoint
+	_, err = client.UpdateWorkloadDefinition(r.APIClient, r.APIServer, workloadDefinition)
+	if err != nil {
+		return fmt.Errorf("failed to update workload definition in threeport API: %w", err)
+	}
+
+	// update gateway definition
+	domainNameDefinitionReconciled := true
+	domainNameDefinition.WorkloadDefinitionID = workloadDefinition.ID
+	domainNameDefinition.Reconciled = &domainNameDefinitionReconciled
+	_, err = client.UpdateDomainNameDefinition(
+		r.APIClient,
+		r.APIServer,
+		domainNameDefinition,
+	)
+	if err != nil {
+		return fmt.Errorf("failed to update domain name definition in threeport API: %w", err)
+	}
+
+	log.V(1).Info(
+		"domian name definition created",
+		"domainNameDefinitionID", workloadDefinition.ID,
+	)
+
 	return nil
 }
 
@@ -35,6 +120,20 @@ func domainNameDefinitionDeleted(
 	domainNameDefinition *v0.DomainNameDefinition,
 	log *logr.Logger,
 ) error {
+
+	// delete workload definition
+	if domainNameDefinition.WorkloadDefinitionID == nil {
+		return fmt.Errorf("failed to delete workload definition, workload definition ID is nil")
+	}
+	_, err := client.DeleteWorkloadDefinition(r.APIClient, r.APIServer, *domainNameDefinition.WorkloadDefinitionID)
+	if err != nil {
+		return fmt.Errorf("failed to delete workload definition with ID %d: %w", *domainNameDefinition.WorkloadDefinitionID, err)
+	}
+
+	log.V(1).Info(
+		"gateway definition deleted",
+		"gatewayDefinitionID", domainNameDefinition.ID,
+	)
 
 	return nil
 }
--- pkg/api/v0/gateway.go
+++ pkg/api/v0/gateway.go
@@ -99,6 +99,9 @@ type DomainNameDefinition struct {
 	// Type *string `json:"Type,omitempty" query:"type" gorm:"default:'A'"
 	// validate:"optional"`
 
+	// The workload definition that belongs to this resource.
+	WorkloadDefinitionID *uint `json:"WorkloadDefinitionID,omitempty" query:"workloaddefinitionid" validate:"optional"`
+
 	// The associated domain name instances that are deployed from this definition.
 	DomainNameInstances []*DomainNameInstance `json:"DomainNameInstances,omitempty" validate:"optional,association"`
 
