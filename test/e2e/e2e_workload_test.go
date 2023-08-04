6b12e4e5220dea053355160f2d5b02392af73c0c
Randy Johnson <randy@qleet.io>
2023-08-04 16:47:39 -0400

    fix: revert changes to e2e test

--- test/e2e/e2e_workload_test.go
+++ test/e2e/e2e_workload_test.go
@@ -309,7 +309,7 @@ func TestWorkloadE2E(t *testing.T) {
 		// check kube cluster for expected resources
 		allResourcesFound := false
 		findAttempts := 0
-		findAttemptsMax := 360
+		findAttemptsMax := 60
 		findCheckDurationSeconds := 1
 		for findAttempts < findAttemptsMax {
 			resourcesFound := 0
@@ -395,7 +395,7 @@ func TestWorkloadE2E(t *testing.T) {
 		// check to make sure kube resources are gone
 		allResourcesGone := false
 		goneAttempts := 0
-		goneAttemptsMax := 360
+		goneAttemptsMax := 30
 		goneCheckDurationSeconds := 1
 		for goneAttempts < goneAttemptsMax {
 			resourcesGone := 0
