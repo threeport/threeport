8c97ac91163bebb81d0bc6c98854a6e1cd83a03f
Rich Lander <lander2k2@protonmail.com>
2023-08-04 15:10:50 -0400

    fix: remove probes when deploying threeport-agent
    Signed-off-by: Rich Lander <lander2k2@protonmail.com>

--- internal/threeport/components.go
+++ internal/threeport/components.go
@@ -995,25 +995,23 @@ func InstallThreeportAgent(
 								"args":            agentArgs,
 								"image":           agentImage,
 								"imagePullPolicy": "IfNotPresent",
+								//"livenessProbe": map[string]interface{}{
+								//	"httpGet": map[string]interface{}{
+								//		"path": "/healthz",
+								//		"port": 8081,
+								//	},
+								//	"initialDelaySeconds": 5,
+								//	"periodSeconds":       20,
+								//},
 								"name": "manager",
-								// TODO: configure liveness & readiness to be
-								// consistent across develoment controller deployments
-								// "livenessProbe": map[string]interface{}{
-								// 	"httpGet": map[string]interface{}{
-								// 		"path": "/healthz",
-								// 		"port": 8081,
-								// 	},
-								// 	"initialDelaySeconds": 5,
-								// 	"periodSeconds":       20,
-								// },
-								// "readinessProbe": map[string]interface{}{
-								// 	"httpGet": map[string]interface{}{
-								// 		"path": "/readyz",
-								// 		"port": 8081,
-								// 	},
-								// 	"initialDelaySeconds": 5,
-								// 	"periodSeconds":       10,
-								// },
+								//"readinessProbe": map[string]interface{}{
+								//	"httpGet": map[string]interface{}{
+								//		"path": "/readyz",
+								//		"port": 8081,
+								//	},
+								//	"initialDelaySeconds": 5,
+								//	"periodSeconds":       10,
+								//},
 								//"resources": map[string]interface{}{
 								//	"limits": map[string]interface{}{
 								//		"cpu":    "500m",
