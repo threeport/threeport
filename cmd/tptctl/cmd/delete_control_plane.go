29a1a49723627cdcd90766d6989cabb094aa7a83
Randy Johnson <randy@qleet.io>
2023-08-09 15:33:12 -0400

    fix: add --aws-config-profile and --aws-region flags to tptctl delete control plane

--- cmd/tptctl/cmd/delete_control_plane.go
+++ cmd/tptctl/cmd/delete_control_plane.go
@@ -33,5 +33,13 @@ func init() {
 		&cliArgs.InstanceName,
 		"name", "n", "", "Required. Name of control plane instance.",
 	)
+	DeleteControlPlaneCmd.Flags().StringVar(
+		&cliArgs.AwsConfigProfile,
+		"aws-config-profile", "default", "The AWS config profile to draw credentials from when using eks provider.",
+	)
+	DeleteControlPlaneCmd.Flags().StringVar(
+		&cliArgs.AwsRegion,
+		"aws-region", "", "AWS region code to install threeport in when using eks provider. If provided, will take precedence over AWS config profile and environment variables.",
+	)
 	DeleteControlPlaneCmd.MarkFlagRequired("name")
 }
