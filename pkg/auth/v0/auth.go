17fd054ac6e196deffd869e4746782b93af63464
Randy Johnson <randy@qleet.io>
2023-08-17 20:16:37 -0400

    feat: add GenerateKey, Encrypt, and Decrypt funcs

--- pkg/auth/v0/auth.go
+++ pkg/auth/v0/auth.go
@@ -2,12 +2,16 @@ package auth
 
 import (
 	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
 	"crypto/x509/pkix"
+	"encoding/base64"
 	"encoding/pem"
 	"fmt"
+	"io"
 	"math/big"
 	"net"
 	"net/url"
@@ -181,4 +185,75 @@ func GetPEMEncoding(cert []byte, encodingType string) (pemEncodingString string)
 	})
 
 	return pemEncoding.String()
+}
+
+func GenerateKey() (string, error) {
+	key := make([]byte, 64)
+
+	_, err := rand.Read(key)
+	if err != nil {
+		return "", fmt.Errorf("failed to generate key: %w", err)
+	}
+
+	return base64.StdEncoding.EncodeToString(key), nil
+}
+
+func Encrypt(key, text string) {
+
+	c, err := aes.NewCipher([]byte(key))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// gcm or Galois/Counter Mode, is a mode of operation
+	// for symmetric key cryptographic block ciphers
+	// - https://en.wikipedia.org/wiki/Galois/Counter_Mode
+	gcm, err := cipher.NewGCM(c)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// creates a new byte array the size of the nonce
+	// which must be passed to Seal
+	nonce := make([]byte, gcm.NonceSize())
+
+	// populates our nonce with a cryptographically secure
+	// random sequence
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		fmt.Println(err)
+	}
+
+	// here we encrypt our text using the Seal function
+	// Seal encrypts and authenticates plaintext, authenticates the
+	// additional data and appends the result to dst, returning the updated
+	// slice. The nonce must be NonceSize() bytes long and unique for all
+	// time, for a given key.
+	fmt.Println(gcm.Seal(nonce, nonce, []byte(text), nil))
+
+}
+
+func Decrypt(key, ciphertext string) {
+
+	c, err := aes.NewCipher([]byte(key))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	gcm, err := cipher.NewGCM(c)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	nonceSize := gcm.NonceSize()
+	if len(ciphertext) < nonceSize {
+		fmt.Println(err)
+	}
+
+	nonce, ciphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
+	plaintext, err := gcm.Open(nil, []byte(nonce), []byte(ciphertext), nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(plaintext))
+
 }
