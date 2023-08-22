770ba543f860ad5beeee20d819c0723090c38acb
Rich Lander <lander2k2@protonmail.com>
2023-08-21 23:18:59 -0400

    release: cut version v0.2.0
    Signed-off-by: Rich Lander <lander2k2@protonmail.com>

--- cmd/rest-api/main.go
+++ cmd/rest-api/main.go
@@ -30,7 +30,7 @@ import (
 )
 
 // @title Threeport RESTful API
-// @version v0.1.3
+// @version v0.2.0
 // @description Threeport RESTful API.
 // @termsOfService https://threeport.io/api-tos/
 // @contact.name Threeport Admin
