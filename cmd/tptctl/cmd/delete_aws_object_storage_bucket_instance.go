0eb2a23c35bd1ec6884f7867204fb733d27df7fe
Rich Lander <lander2k2@protonmail.com>
2023-09-19 08:56:04 -0400

    fix: add usage of requested instance to new tptctl commands
    Signed-off-by: Rich Lander <lander2k2@protonmail.com>

--- cmd/tptctl/cmd/delete_aws_object_storage_bucket_instance.go
+++ cmd/tptctl/cmd/delete_aws_object_storage_bucket_instance.go
@@ -26,13 +26,13 @@ var DeleteAwsObjectStorageBucketInstanceCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 
 		// get threeport config and extract threeport API endpoint
-		threeportConfig, err := config.GetThreeportConfig(cliArgs.InstanceName)
+		threeportConfig, requestedInstance, err := config.GetThreeportConfig(cliArgs.InstanceName)
 		if err != nil {
 			cli.Error("failed to get threeport config", err)
 			os.Exit(1)
 		}
 
-		apiEndpoint, err := threeportConfig.GetThreeportAPIEndpoint()
+		apiEndpoint, err := threeportConfig.GetThreeportAPIEndpoint(requestedInstance)
 		if err != nil {
 			cli.Error("failed to get threeport API endpoint from config", err)
 			os.Exit(1)
@@ -45,12 +45,12 @@ var DeleteAwsObjectStorageBucketInstanceCmd = &cobra.Command{
 		}
 
 		// get threeport API client
-		cliArgs.AuthEnabled, err = threeportConfig.GetThreeportAuthEnabled()
+		cliArgs.AuthEnabled, err = threeportConfig.GetThreeportAuthEnabled(requestedInstance)
 		if err != nil {
 			cli.Error("failed to determine if auth is enabled on threeport API", err)
 			os.Exit(1)
 		}
-		ca, clientCertificate, clientPrivateKey, err := threeportConfig.GetThreeportCertificates()
+		ca, clientCertificate, clientPrivateKey, err := threeportConfig.GetThreeportCertificatesForInstance(requestedInstance)
 		if err != nil {
 			cli.Error("failed to get threeport certificates from config", err)
 			os.Exit(1)
