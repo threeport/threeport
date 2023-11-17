88c7232218a140219a71bd1cdbedf09b089e720c
Randy Johnson <rj9317@gmail.com>
2023-11-17 00:48:20 -0500

    feat: add TimePtr util function

--- pkg/util/v0/ptr.go
+++ pkg/util/v0/ptr.go
@@ -1,6 +1,13 @@
 package v0
 
-// Ptr returns a pointer to the bool value passed in.
+import "time"
+
+// BoolPtr returns a pointer to the bool value passed in.
 func BoolPtr(b bool) *bool {
 	return &b
+}
+
+// TimePtr returns a pointer to the time value passed in.
+func TimePtr(t time.Time) *time.Time {
+	return &t
 }
