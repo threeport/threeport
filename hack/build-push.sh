ba496b9d5226ede778ecd424fdb27debd5597a06
Rich Lander <lander2k2@protonmail.com>
2024-01-12 14:53:59 -0500

    chore: add hack to build and push images for helm
    Signed-off-by: Rich Lander <lander2k2@protonmail.com>

---
+++ hack/build-push.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+./bin/tptdev build -r $TEST_REPO -t $TEST_TAG --push --names aws-controller \
+    && ./bin/tptdev build -r $TEST_REPO -t $TEST_TAG --push --names control-plane-controller \
+    && ./bin/tptdev build -r $TEST_REPO -t $TEST_TAG --push --names gateway-controller \
+    && ./bin/tptdev build -r $TEST_REPO -t $TEST_TAG --push --names kubernetes-runtime-controller \
+    && ./bin/tptdev build -r $TEST_REPO -t $TEST_TAG --push --names rest-api \
+    && ./bin/tptdev build -r $TEST_REPO -t $TEST_TAG --push --names workload-controller \
+    && ./bin/tptdev build -r $TEST_REPO -t $TEST_TAG --push --names helm-workload-controller
+
