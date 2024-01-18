6a6b6bc590739df6cdb723218840a9580f7b12f6
Randy Johnson <rj9317@gmail.com>
2024-01-18 10:02:48 -0500

    refactor: break apart default grafana values

--- internal/observability/helm_values.go
+++ internal/observability/helm_values.go
@@ -1,39 +1,10 @@
 package observability
 
 const grafanaValues = `
-## Configure anonymous authentication
-## https://grafana.com/docs/grafana/latest/setup-grafana/configure-security/configure-authentication/grafana/#anonymous-authentication
-grafana.ini:
-  auth.anonymous:
-    enabled: true
-    org_name: Main Org.
-    org_role: Admin
-
 persistence:
   enabled: true
 
-datasources:
-  loki-datasource.yaml:
-    apiVersion: 1
-    datasources:
-    - name: loki
-      access: proxy
-      # basicAuth: true
-      # basicAuthPassword: pass
-      # basicAuthUser: daco
-      editable: false
-      isDefault: false
-      jsonData:
-          tlsSkipVerify: true
-      type: loki
-      url: http://loki:3100
-
 adminPassword: password
-
-rbac:
-  ## If true, Grafana PSPs will be created
-  ##
-  pspEnabled: false
 
 sidecar:
   dashboards:
@@ -49,7 +20,24 @@ sidecar:
     labelValue: "1"
     # Allow discovery in all namespaces for dashboards
     searchNamespace: ALL
+`
 
+const grafanaLokiDatasource = `
+datasources:
+  loki-datasource.yaml:
+    apiVersion: 1
+    datasources:
+    - name: loki
+      access: proxy
+      editable: false
+      isDefault: false
+      jsonData:
+          tlsSkipVerify: true
+      type: loki
+      url: http://loki:3100
+`
+
+const grafanaPrometheusServiceMonitor = `
 serviceMonitor:
   # If true, a ServiceMonitor CRD is created for a prometheus operator
   # https://github.com/coreos/prometheus-operator
@@ -60,9 +48,8 @@ serviceMonitor:
   #
   interval: ""
 `
+
 const kubePrometheusStackValues = `
-## Using default values from https://github.com/grafana/helm-charts/blob/main/charts/grafana/values.yaml
-##
 grafana:
   enabled: false
 
