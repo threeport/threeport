53d77e1b972576678cb6f4d480aa16beeb0e720f
Randy Johnson <rj9317@gmail.com>
2024-01-24 19:25:54 -0500

    fix: use util.BoolPtr

--- internal/observability/metrics_instance_gen.go
+++ internal/observability/metrics_instance_gen.go
@@ -249,10 +249,9 @@ func MetricsInstanceReconciler(r *controller.Reconciler) {
 
 			// set the object's Reconciled field to true if not deleted
 			if notif.Operation != notifications.NotificationOperationDeleted {
-				objectReconciled := true
 				reconciledMetricsInstance := v0.MetricsInstance{
 					Common:         v0.Common{ID: metricsInstance.ID},
-					Reconciliation: v0.Reconciliation{Reconciled: &objectReconciled},
+					Reconciliation: v0.Reconciliation{Reconciled: util.BoolPtr(true)},
 				}
 				updatedMetricsInstance, err := client.UpdateMetricsInstance(
 					r.APIClient,
