6bb1f1ed42cec7d5e4f31614f011ffe46b53dba2
Randy Johnson <rj9317@gmail.com>
2024-01-30 23:17:33 -0500

    refactor: remove util.Operations from metrics reconciler

--- internal/observability/metrics_definition.go
+++ internal/observability/metrics_definition.go
@@ -11,14 +11,6 @@ import (
 	util "github.com/threeport/threeport/pkg/util/v0"
 )
 
-// MetricsDefinitionConfig is the configuration for a metrics definition
-// reconciler.
-type MetricsDefinitionConfig struct {
-	r                 *controller.Reconciler
-	metricsDefinition *v0.MetricsDefinition
-	log               *logr.Logger
-}
-
 // metricsDefinitionCreated reconciles state for a new kubernetes
 // runtime instance.
 func metricsDefinitionCreated(
@@ -26,22 +18,28 @@ func metricsDefinitionCreated(
 	metricsDefinition *v0.MetricsDefinition,
 	log *logr.Logger,
 ) (int64, error) {
-	c := &MetricsDefinitionConfig{
-		r:                 r,
-		metricsDefinition: metricsDefinition,
-		log:               log,
+
+	// create kube-prometheus-stack helm workload definition
+	kubePrometheusStackHelmWorkloadDefinition, err := client.CreateHelmWorkloadDefinition(
+		r.APIClient,
+		r.APIServer,
+		&v0.HelmWorkloadDefinition{
+			Definition: v0.Definition{
+				Name: util.StringPtr(KubePrometheusStackChartName(*metricsDefinition.Name)),
+			},
+			Repo:               util.StringPtr(PrometheusCommunityHelmRepo),
+			Chart:              util.StringPtr("kube-prometheus-stack"),
+			HelmChartVersion:   metricsDefinition.KubePrometheusStackHelmChartVersion,
+			HelmValuesDocument: metricsDefinition.KubePrometheusStackHelmValuesDocument,
+		})
+	if err != nil {
+		return 0, fmt.Errorf("failed to create kube-prometheus-stack helm workload definition: %w", err)
 	}
 
-	// get metrics definition operations
-	operations := getMetricsDefinitionOperations(c)
-
-	// execute create metrics definition operations
-	if err := operations.Create(); err != nil {
-		return 0, fmt.Errorf("failed to execute create metrics definition operations: %w", err)
-	}
+	// update metrics definition with helm workload definition id
+	metricsDefinition.KubePrometheusStackHelmWorkloadDefinitionID = kubePrometheusStackHelmWorkloadDefinition.ID
 
 	// update metrics instance reconciled field
-	metricsDefinition.Reconciled = util.BoolPtr(true)
 	if _, err := client.UpdateMetricsDefinition(
 		r.APIClient,
 		r.APIServer,
@@ -70,83 +68,14 @@ func metricsDefinitionDeleted(
 	metricsDefinition *v0.MetricsDefinition,
 	log *logr.Logger,
 ) (int64, error) {
-	c := &MetricsDefinitionConfig{
-		r:                 r,
-		metricsDefinition: metricsDefinition,
-		log:               log,
-	}
-
-	// get metrics definition operations
-	operations := getMetricsDefinitionOperations(c)
-
-	// execute delete metrics definition operations
-	if err := operations.Delete(); err != nil {
-		return 0, fmt.Errorf("failed to execute delete metrics definition operations: %w", err)
+	// delete kube-prometheus-stack helm workload definition
+	if _, err := client.DeleteHelmWorkloadDefinition(
+		r.APIClient,
+		r.APIServer,
+		*metricsDefinition.KubePrometheusStackHelmWorkloadDefinitionID,
+	); err != nil && !errors.Is(err, client.ErrObjectNotFound) {
+		return 0, fmt.Errorf("failed to delete kube-prometheus-stack helm workload definition: %w", err)
 	}
 
 	return 0, nil
-}
-
-func getMetricsDefinitionOperations(c *MetricsDefinitionConfig) *util.Operations {
-	operations := util.Operations{}
-
-	// append kube-prometheus-stack operations
-	operations.AppendOperation(util.Operation{
-		Name: "kube-prometheus-stack",
-		Create: func() error {
-			if err := c.createKubePrometheusStackHelmWorkloadDefinition(); err != nil {
-				return fmt.Errorf("failed to create loki helm workload instance: %w", err)
-			}
-			return nil
-		},
-		Delete: func() error {
-			if err := c.deleteKubePrometheusStackHelmWorkloadDefinition(); err != nil {
-				return fmt.Errorf("failed to delete loki helm workload instance: %w", err)
-			}
-			return nil
-		},
-	})
-
-	return &operations
-}
-
-// createKubePrometheusStackHelmWorkloadDefinition creates a kube-prometheus-stack helm
-// workload definition.
-func (c *MetricsDefinitionConfig) createKubePrometheusStackHelmWorkloadDefinition() error {
-	// create kube-prometheus-stack helm workload definition
-	kubePrometheusStackHelmWorkloadDefinition, err := client.CreateHelmWorkloadDefinition(
-		c.r.APIClient,
-		c.r.APIServer,
-		&v0.HelmWorkloadDefinition{
-			Definition: v0.Definition{
-				Name: util.StringPtr(KubePrometheusStackChartName(*c.metricsDefinition.Name)),
-			},
-			Repo:           util.StringPtr(PrometheusCommunityHelmRepo),
-			Chart:          util.StringPtr("kube-prometheus-stack"),
-			HelmChartVersion:   c.metricsDefinition.KubePrometheusStackHelmChartVersion,
-			HelmValuesDocument: c.metricsDefinition.KubePrometheusStackHelmValuesDocument,
-		})
-	if err != nil {
-		return fmt.Errorf("failed to create kube-prometheus-stack helm workload definition: %w", err)
-	}
-
-	// update metrics definition with helm workload definition id
-	c.metricsDefinition.KubePrometheusStackHelmWorkloadDefinitionID = kubePrometheusStackHelmWorkloadDefinition.ID
-
-	return nil
-}
-
-// deleteKubePrometheusStackHelmWorkloadDefinition deletes a kube-prometheus-stack helm
-// workload definition.
-func (c *MetricsDefinitionConfig) deleteKubePrometheusStackHelmWorkloadDefinition() error {
-	// delete kube-prometheus-stack helm workload definition
-	if _, err := client.DeleteHelmWorkloadDefinition(
-		c.r.APIClient,
-		c.r.APIServer,
-		*c.metricsDefinition.KubePrometheusStackHelmWorkloadDefinitionID,
-	); err != nil && !errors.Is(err, client.ErrObjectNotFound) {
-		return fmt.Errorf("failed to delete kube-prometheus-stack helm workload definition: %w", err)
-	}
-
-	return nil
 }
