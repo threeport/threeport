1a64b7d8d3524df564602089ab640b716693f0bd
Randy Johnson <rj9317@gmail.com>
2024-02-08 19:10:33 -0500

    feate: add CreateExternalSecret()

--- internal/secret/secret_instance.go
+++ internal/secret/secret_instance.go
@@ -2,6 +2,7 @@ package secret
 
 import (
 	"github.com/go-logr/logr"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 
 	v0 "github.com/threeport/threeport/pkg/api/v0"
 	controller "github.com/threeport/threeport/pkg/controller/v0"
@@ -35,4 +36,36 @@ func secretInstanceDeleted(
 	log *logr.Logger,
 ) (int64, error) {
 	return 0, nil
+}
+
+// CreateExternalSecret returns a new ExternalSecret object
+func CreateExternalSecret(name string) *unstructured.Unstructured {
+	var externalSecret = &unstructured.Unstructured{
+		Object: map[string]interface{}{
+			"apiVersion": "external-secrets.io/v1beta1",
+			"kind":       "ExternalSecret",
+			"metadata": map[string]interface{}{
+				"name": name,
+			},
+			"spec": map[string]interface{}{
+				"refreshInterval": "1h",
+				"secretStoreRef": map[string]interface{}{
+					"name": "aws-secretsmanager",
+					"kind": "SecretStore",
+				},
+				"target": map[string]interface{}{
+					"name":           name,
+					"creationPolicy": "Owner",
+				},
+				"dataFrom": []interface{}{
+					map[string]interface{}{
+						"extract": map[string]interface{}{
+							"key": name,
+						},
+					},
+				},
+			},
+		},
+	}
+	return externalSecret
 }
