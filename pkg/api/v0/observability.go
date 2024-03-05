68cf5057bafe26aea3cecf0d4e0878098d3e8b9d
Randy Johnson <rj9317@gmail.com>
2024-03-05 11:55:34 -0500

    feat: add MetricsDefinition and MetricsInstance types

--- pkg/api/v0/observability.go
+++ pkg/api/v0/observability.go
@@ -152,18 +152,21 @@ type MetricsDefinition struct {
 	Definition     `mapstructure:",squash"`
 	Reconciliation `mapstructure:",squash"`
 
-	// The kube-prometheus-stack Helm workload definition that belongs to this resource.
-	KubePrometheusStackHelmWorkloadDefinitionID *uint `json:"KubePrometheusStackHelmWorkloadDefinitionID,omitempty" query:"kubeprometheusstackhelmworkloaddefinitionid" validate:"optional"`
+	// The domain name to serve requests for.
+	DomainNameDefinitionID *uint `json:"DomainNameDefinitionID,omitempty" query:"domainnamedefinition" validate:"optional"`
 
-	// The version of the kube-prometheus-stack helm chart to use from the helm repo, e.g. 1.2.3
-	KubePrometheusStackHelmChartVersion *string `json:"KubePrometheusStackHelmChartVersion,omitempty" query:"kubeprometheusstackhelmchartversion" gorm:"default:'55.8.1'" validate:"optional"`
+	// An optional subdomain to add to the domain name.
+	SubDomain *string `json:"SubDomain,omitempty" query:"subdomain" validate:"optional" gorm:"default:'metrics'"`
+
+	// The Helm workload definitions that belongs to this resource.
+	HelmWorkloadDefinitionIds []*uint `json:"HelmWorkloadDefinitionId,omitempty" query:"helmworkloaddefinitionid" validate:"optional"`
 
 	// Optional Helm workload definition values that can be provided to configure the
-	// underlying kube-prometheus-stack chart.
-	KubePrometheusStackHelmValuesDocument *string `json:"KubePrometheusStackHelmValuesDocument,omitempty" query:"kubeprometheusstackhelmvaluesdocument" validate:"optional"`
+	// underlying charts.
+	HelmWorkloadDefinitionValues *string `json:"HelmWorkloadDefinitionValues,omitempty" query:"helmworkloaddefinitionvalues" validate:"optional"`
 
-	// The associated metrics instances that are deployed from this definition.
-	MetricsInstances []*MetricsInstance `json:"MetricsInstances,omitempty" validate:"optional,association"`
+	// The associated gateway instance that are deployed from this definition.
+	GatewayInstance *GatewayInstance `json:"GatewayInstances,omitempty" validate:"optional,association"`
 }
 
 // +threeport-sdk:reconciler
@@ -173,14 +176,17 @@ type MetricsInstance struct {
 	Instance       `mapstructure:",squash"`
 	Reconciliation `mapstructure:",squash"`
 
-	// MetricsDefinitionID is the definition used to configure the workload instance.
-	MetricsDefinitionID *uint `json:"MetricsDefinitionID,omitempty" query:"metricsdefinitionid" gorm:"not null" validate:"required"`
-
 	// The kubernetes runtime where the ingress layer is installed.
 	KubernetesRuntimeInstanceID *uint `json:"KubernetesRuntimeInstanceID,omitempty" query:"kubernetesruntimeinstanceid" gorm:"not null" validate:"required"`
 
-	// The kube-prometheus-stack Helm workload definition that belongs to this resource.
-	KubePrometheusStackHelmWorkloadInstanceID *uint `json:"KubePrometheusStackHelmWorkloadInstanceID,omitempty" query:"kubeprometheusstackhelmworkloadinstanceid" validate:"optional"`
+	// The domain name instance to serve requests for.
+	// DomainNameInstanceID *uint `json:"DomainNameInstanceID,omitempty" query:"domainnameinstanceid" validate:"optional"`
+
+	// GatewayDefinitionID is the definition used to configure the workload instance.
+	GatewayDefinitionID *uint `json:"GatewayDefinitionID,omitempty" query:"gatewaydefinitionid" gorm:"not null" validate:"required"`
+
+	// The workload instance this gateway belongs to.
+	HelmWorkloadInstanceIDs []*uint `json:"HelmWorkloadInstanceID,omitempty" query:"helmworkloadinstanceid" gorm:"not null" validate:"required"`
 
 	// Optional Helm workload definition values that can be provided to configure the
 	// underlying kube-prometheus-stack chart.
