9065f3ef7412a0bce984ba12dd936b4703aa5831
Randy Johnson <rj9317@gmail.com>
2024-03-14 20:03:18 -0400

    refactor: split attached object reference database migrations into three transactions

--- cmd/database-migrator/migrations/00007_update_attached_object_reference_rows.go
+++ cmd/database-migrator/migrations/00007_update_attached_object_reference_rows.go
@@ -48,42 +48,10 @@ func Up00007(ctx context.Context, tx *sql.Tx) error {
 		return fmt.Errorf("failed to set object_id to workload_instance_id: %w", err)
 	}
 
-	// remove the `type` column
-	if _, err := tx.ExecContext(ctx, `
-		ALTER TABLE attached_object_references
-		DROP COLUMN type;
-	`); err != nil {
-		return fmt.Errorf("failed to drop type column: %w", err)
-	}
-
-	// remove the `workload_instance_id` column
-	if _, err := tx.ExecContext(ctx, `
-		ALTER TABLE attached_object_references
-		DROP COLUMN workload_instance_id;
-	`); err != nil {
-		return fmt.Errorf("failed to drop type column: %w", err)
-	}
-
 	return nil
 }
 
 func Down00007(ctx context.Context, tx *sql.Tx) error {
-	// add the 'workload_instance_id' column
-	if _, err := tx.ExecContext(ctx, `
-		ALTER TABLE attached_object_references
-		ADD COLUMN workload_instance_id bigint;
-	`); err != nil {
-		return fmt.Errorf("failed to add workload_instance_id column: %w", err)
-	}
-
-	// add the `type` column
-	if _, err := tx.ExecContext(ctx, `
-		ALTER TABLE attached_object_references
-		ADD COLUMN type varchar(255);
-	`); err != nil {
-		return fmt.Errorf("failed to add type column: %w", err)
-	}
-
 	// map object_id -> workload_instance_id
 	if _, err := tx.ExecContext(ctx, `
 		UPDATE attached_object_references
