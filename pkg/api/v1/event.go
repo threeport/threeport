e80c7ba76adc3bbb7f23e6222bd1e20364061788
Randy Johnson <rj9317@gmail.com>
2024-04-22 13:15:35 -0400

    feat: add EventRecorder

--- pkg/api/v1/event.go
+++ pkg/api/v1/event.go
@@ -47,3 +47,23 @@ type Event struct {
 	// ID of the controller instance, e.g. `kubelet-xyzf`.
 	ReportingInstance string `validate:"optional"`
 }
+
+// EventRecorder records events to the backend.
+type EventRecorder struct {
+
+	// The component reporting this event. Should be a short machine understandable string.
+	Source string `validate:"optional"`
+
+	// Name of the controller that emitted this Event, e.g. `kubernetes.io/kubelet`.
+	ReportingController string `validate:"optional"`
+
+	// ID of the controller instance, e.g. `kubelet-xyzf`.
+	ReportingInstance string `validate:"optional"`
+}
+
+// Event records a new event with the given information.
+func (*EventRecorder) Event() error {
+	// if event exists, increment its count by 1
+	// else, add new event
+	return nil
+}
