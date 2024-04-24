e52986c86d7c00bd10edb9280282dd7a8b4114fc
Randy Johnson <rj9317@gmail.com>
2024-04-24 16:34:05 -0400

    fix: move event.go back to pkg/api/v0

--- pkg/api/v0/event.go
+++ pkg/api/v0/event.go
@@ -1,11 +1,8 @@
-package v1
+package v0
 
 import "time"
 
 type Event struct {
-
-	// The object that this event refers to.
-	AttachedObject *AttachedObjectReference
 
 	// A short, machine understandable string that gives the reason for the event being generated.
 	Reason string `validate:"optional"`
@@ -16,20 +13,17 @@ type Event struct {
 	// The component reporting this event. Should be a short machine understandable string.
 	Source string `validate:"optional"`
 
-	// The time at which the event was first recorded.
-	FirstTimestamp time.Time `validate:"optional"`
-
-	// The time at which the most recent occurrence of this event was recorded.
-	LastTimestamp time.Time `validate:"optional"`
-
 	// The number of times this event has occurred.
 	Count int32 `validate:"optional"`
 
-	// Type of this event (Normal, Warning), new types could be added in the future.
-	Type string `validate:"optional"`
-
 	// Time when this Event was first observed.
 	EventTime time.Time `validate:"optional"`
+
+	// The time at which the most recent occurrence of this event was recorded.
+	LastObservedTime time.Time `validate:"optional"`
+
+	// Type of this event (Normal, Warning), new types could be added in the future.
+	Type string `validate:"optional"`
 
 	// Data about the Event series this event represents or nil if it's a singleton Event.
 	// Series *EventSeries `validate:"optional"`
@@ -46,24 +40,4 @@ type Event struct {
 
 	// ID of the controller instance, e.g. `kubelet-xyzf`.
 	ReportingInstance string `validate:"optional"`
-}
-
-// EventRecorder records events to the backend.
-type EventRecorder struct {
-
-	// The component reporting this event. Should be a short machine understandable string.
-	Source string `validate:"optional"`
-
-	// Name of the controller that emitted this Event, e.g. `kubernetes.io/kubelet`.
-	ReportingController string `validate:"optional"`
-
-	// ID of the controller instance, e.g. `kubelet-xyzf`.
-	ReportingInstance string `validate:"optional"`
-}
-
-// Event records a new event with the given information.
-func (*EventRecorder) Event() error {
-	// if event exists, increment its count by 1
-	// else, add new event
-	return nil
 }
--- pkg/controller/v0/reconcile.go
+++ pkg/controller/v0/reconcile.go
@@ -10,8 +10,8 @@ import (
 	"github.com/go-logr/logr"
 	"github.com/google/uuid"
 	"github.com/nats-io/nats.go"
+	"github.com/threeport/threeport/internal/event"
 	v0 "github.com/threeport/threeport/pkg/api/v0"
-	v1 "github.com/threeport/threeport/pkg/api/v1"
 )
 
 // ReconcilerConfig contains values needed to start new reconcilers in
@@ -82,7 +82,7 @@ type Reconciler struct {
 	EncryptionKey string
 
 	// EventRecorder is the event recorder used to record events.
-	EventRecorder *v1.EventRecorder
+	EventRecorder *event.EventRecorder
 }
 
 // PullMessage checks the queue for a message and returns it if there was a
