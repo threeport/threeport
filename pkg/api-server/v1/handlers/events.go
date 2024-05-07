2d3923223638a68f66e93cf5a4c90096103d9824
Randy Johnson <rj9317@gmail.com>
2024-05-07 19:11:54 -0400

    feat: throw 403 when objectId is not provided

--- pkg/api-server/v1/handlers/events.go
+++ pkg/api-server/v1/handlers/events.go
@@ -7,16 +7,17 @@ import (
 	v1 "github.com/threeport/threeport/pkg/api/v1"
 )
 
-// @Summary gets all events.
-// @Description Get all events from the Threeport database.
-// @ID get-v1-events
+// @Summary gets all events joined with attached object references.
+// @Description Get all events joined with attached object references
+// from the Threeport database for a given objectId.
+// @ID get-v1-events-join-attached-object-references
 // @Accept json
 // @Produce json
-// @Param name query string false "event search by name"
+// @Param name query string false "events joined with attached object references search by objectId"
 // @Success 200 {object} v0.Response "OK"
 // @Failure 400 {object} v0.Response "Bad Request"
 // @Failure 500 {object} v0.Response "Internal Server Error"
-// @Router /v1/events [GET]
+// @Router /v1/events-join-attached-object-references [GET]
 func (h Handler) GetEventsJoinAttachedObjectReferences(c echo.Context) error {
 	objectType := v1.ObjectTypeEvent
 	params, err := c.(*iapi.CustomContext).GetPaginationParams()
@@ -31,11 +32,15 @@ func (h Handler) GetEventsJoinAttachedObjectReferences(c echo.Context) error {
 
 	var totalCount int64
 	records := &[]v1.Event{}
-	objectID := c.QueryParam("objectid")
+	objectId := c.QueryParam("objectid")
+	if objectId == "" {
+		return iapi.ResponseStatus400(c, &params, err, objectType)
+	}
+
 	if result := h.DB.Joins(
 		"INNER JOIN attached_object_references ON events.attached_object_reference_id = attached_object_references.id",
 	).Where(
-		"attached_object_references.object_id = ?", objectID,
+		"attached_object_references.object_id = ?", objectId,
 	).Where(&filter).Find(records); result.Error != nil {
 		return iapi.ResponseStatus500(c, &params, result.Error, objectType)
 	}
