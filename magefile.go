88812ed4ca85099af2118bad8e65ac2a6a8e9633
Anish-1101 <anishathmakoor04@gmail.com>
2024-08-03 23:11:01 +0200

    dev: convert Makefile targets to mage functions

--- magefile.go
+++ magefile.go
@@ -5,7 +5,9 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
+	"path/filepath"
 )
 
 // BuildAgent builds the binary for the workload-controller.
@@ -74,6 +76,134 @@ func BuildImage(
 
 		fmt.Printf("%s image pushed\n", component)
 	}
+
+	return nil
+}
+
+// Builds sdk binary and installs in GOPATH
+
+func BuildSDK() error {
+	goPath := os.Getenv("GOPATH")
+	outputPath := filepath.Join(goPath, "bin", "threeport-sdk")
+
+	sdkCmd := exec.Command(
+		"go",
+		"build",
+		"-o",
+		outputPath,
+		"cmd/sdk/main.go",
+	)
+
+	output, err := sdkCmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("build failed for sdk binary with output: '%s': %w", output, err)
+	}
+
+	fmt.Println("sdk binary built and available at bin/threeport-sdk")
+
+	return nil
+}
+
+// Builds database migrator
+
+func BuildDbMigrator() error {
+
+	buildDbCmd := exec.Command(
+		"go",
+		"build",
+		"-o",
+		"bin/database-migrator",
+		"cmd/database-migrator/main.go",
+	)
+
+	output, err := buildDbCmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("build failed for database migrator with output: '%s': %w", output, err)
+	}
+
+	fmt.Println("database migrator built and available at cmd/database-migrator")
+
+	return nil
+}
+
+// Builds tptdev binary
+
+func BuildTptdev() error {
+
+	buildTptdevCmd := exec.Command(
+		"go",
+		"build",
+		"-o",
+		"bin/tptdev",
+		"cmd/tptdev/main.go",
+	)
+	output, err := buildTptdevCmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("build failed for tptdev with output: '%s': %w", output, err)
+	}
+
+	fmt.Println("tptdev built and available at cmd/tptdev")
+
+	return nil
+}
+
+// Installs tptdev binary
+
+func InstallTptdev() error {
+
+	installTptdevCmd := exec.Command(
+		"sudo",
+		"cp",
+		"./bin/tptdev",
+		"/usr/local/bin/tptdev",
+	)
+	output, err := installTptdevCmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("install failed for tptdev with output: '%s': %w", output, err)
+	}
+
+	fmt.Println("tptdev installed and available at /usr/local/bin/tptdev")
+
+	return nil
+}
+
+// Builds tptctl binary
+
+func BuildTptctl() error {
+
+	buildTptctlCmd := exec.Command(
+		"go",
+		"build",
+		"-o",
+		"bin/tptctl",
+		"cmd/tptctl/main.go",
+	)
+	output, err := buildTptctlCmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("build failed for tptctl binary with output: '%s': %w", output, err)
+	}
+
+	fmt.Println("tptctl binary built and available at cmd/tptctl")
+
+	return nil
+}
+
+// Installs tptctl binary
+
+func InstallTptctl() error {
+
+	installTptctlCmd := exec.Command(
+		"sudo",
+		"cp",
+		"./bin/tptctl",
+		"/usr/local/bin/tptctl",
+	)
+	output, err := installTptctlCmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("install failed for tptctl binary with output: '%s': %w", output, err)
+	}
+
+	fmt.Println("tptctl binary installed and available at/usr/local/bin/tptctl")
 
 	return nil
 }
