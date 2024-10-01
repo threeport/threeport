4a1b4482cae20e215dccdcdd4e651cb35c0ae2d1
Rich Lander <lander2k2@protonmail.com>
2024-10-01 11:42:32 -0400

    fix: apply composite index to attached object table
    Signed-off-by: Rich Lander <lander2k2@protonmail.com>

--- pkg/api/v0/attached_object.go
+++ pkg/api/v0/attached_object.go
@@ -5,14 +5,14 @@ type AttachedObjectReference struct {
 	Common `swaggerignore:"true" mapstructure:",squash"`
 
 	// The object type of the base object.
-	ObjectType *string `json:"ObjectType,omitempty" query:"objecttype" gorm:"not null" validate:"required"`
+	ObjectType *string `json:"ObjectType,omitempty" query:"objecttype" gorm:"not null;uniqueIndex:idx_attached_object_unique" validate:"required"`
 
 	// The object ID of the base object.
-	ObjectID *uint `json:"ObjectID,omitempty" query:"objectid" gorm:"not null" validate:"required"`
+	ObjectID *uint `json:"ObjectID,omitempty" query:"objectid" gorm:"not null;uniqueIndex:idx_attached_object_unique" validate:"required"`
 
 	// The object type of the attached object.
-	AttachedObjectType *string `json:"AttachedObjectType,omitempty" query:"attachedobjecttype" gorm:"not null" validate:"required"`
+	AttachedObjectType *string `json:"AttachedObjectType,omitempty" query:"attachedobjecttype" gorm:"not null;uniqueIndex:idx_attached_object_unique" validate:"required"`
 
 	// The object ID of the attached object.
-	AttachedObjectID *uint `json:"AttachedObjectID,omitempty" query:"attachedobjectid" gorm:"not null" validate:"required"`
+	AttachedObjectID *uint `json:"AttachedObjectID,omitempty" query:"attachedobjectid" gorm:"not null;uniqueIndex:idx_attached_object_unique" validate:"required"`
 }
