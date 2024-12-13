bf0f2cee8e7d08ff03535de09218a77f1dc5dd24
Rich Lander <lander2k2@protonmail.com>
2024-12-13 08:14:31 -0500

    release: cut version v0.6.0
    Signed-off-by: Rich Lander <lander2k2@protonmail.com>

--- cmd/rest-api/main_gen.go
+++ cmd/rest-api/main_gen.go
@@ -30,7 +30,7 @@ import (
 )
 
 // @title Threeport RESTful API
-// @version v0.5.3
+// @version v0.6.0
 // @description Core API server for the Threeport application orchestration control plane.
 // @contact.url https://threerport.io
 // @BasePath /
