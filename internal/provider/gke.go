ac1b222983d54a87b4ef7668fb5b904273adf020
Max Lander <mnlander08@gmail.com>
2026-09-21 16:43:13 -0400

    fix: thread GKE node pool machine type and autoscaling bounds through (#523)
    * fix: thread GKE node pool machine type and autoscaling bounds through
    
    * fix: preserve GKE node pool defaults on the CLI bootstrap path, add mapping test

--- internal/provider/gke.go
+++ internal/provider/gke.go
@@ -45,6 +45,17 @@ type KubernetesRuntimeInfraGKE struct {
 	// The number of nodes initially created for the worker node pool.
 	WorkerNodeInitialCount int32
 
+	// The GCP machine type for the worker node pool (e.g. "e2-standard-2").
+	MachineType string
+
+	// The minimum number of nodes the worker node pool's autoscaler will
+	// scale down to.
+	MinNodeCount int32
+
+	// The maximum number of nodes the worker node pool's autoscaler will
+	// scale up to.
+	MaxNodeCount int32
+
 	// The email address of the GCP service account created for Threeport.
 	// This service account is used with Workload Identity to allow
 	// Threeport controllers to manage GCP resources.
@@ -268,7 +279,7 @@ func (i *KubernetesRuntimeInfraGKE) pulumiProgram() pulumi.RunFunc {
 			NodeCount: pulumi.Int(int(i.WorkerNodeInitialCount)),
 
 			NodeConfig: &gkecontainer.NodePoolNodeConfigArgs{
-				MachineType: pulumi.String("e2-medium"),
+				MachineType: pulumi.String(i.MachineType),
 				DiskSizeGb:  pulumi.Int(50),
 				DiskType:    pulumi.String("pd-standard"),
 				OauthScopes: pulumi.StringArray{
@@ -284,8 +295,8 @@ func (i *KubernetesRuntimeInfraGKE) pulumiProgram() pulumi.RunFunc {
 			},
 
 			Autoscaling: &gkecontainer.NodePoolAutoscalingArgs{
-				MinNodeCount:   pulumi.Int(1),
-				MaxNodeCount:   pulumi.Int(10),
+				MinNodeCount:   pulumi.Int(int(i.MinNodeCount)),
+				MaxNodeCount:   pulumi.Int(int(i.MaxNodeCount)),
 				LocationPolicy: pulumi.String("ANY"),
 			},
 
