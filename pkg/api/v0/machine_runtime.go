f0a93b6d47a718353dd7c7d2a27bb29a0c58d7be
Randy Johnson <rj9317@gmail.com>
2026-09-24 12:16:08 -0600

    fix: store the machine resource inventory the field test reads

--- pkg/api/v0/machine_runtime.go
+++ pkg/api/v0/machine_runtime.go
@@ -1,5 +1,7 @@
 package v0
 
+import "gorm.io/datatypes"
+
 // MachineRuntimeDefinition is the configuration for a machine runtime.  It
 // serves as a template for provisioning machine runtime instances.
 type MachineRuntimeDefinition struct {
@@ -57,6 +59,10 @@ type MachineRuntimeInstance struct {
 
 	// The provider subnet identifier the machine attaches to
 	SubnetID *string `validate:"optional"`
+
+	// An inventory of all provider resources backing this machine, used for
+	// crash recovery and deprovisioning.
+	ResourceInventory *datatypes.JSON `validate:"optional"`
 
 	// The machine runtime definition for this instance.  Optional because
 	// imported machines may not have an associated definition.
